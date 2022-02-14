8f99ea47e00fef15320aa761b3d21092e664b33b
amirhossein ahmadi <amirthehossein@gmail.com>
2022-02-14 09:46:50 +0330

    added userapp and its migrations to the repository

---
+++ src/homework/userapp/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+
+
+class User(models.Model):
+
+    class Meta:
+        abstract = True
+
+    username = models.CharField('username', max_length=32, unique=True,
+                                blank=False, null=False)
+    name = models.CharField('real name', max_length=128, blank=False,
+                            null=False)
+    registration_date = models.DateField('registration date', null=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Teacher(User):
+    pass
+
+
+class Student(User):
+    pass
