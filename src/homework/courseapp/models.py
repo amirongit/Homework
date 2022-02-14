d45272556c49249e15a022d52a96135e28198e5d
amirhossein ahmadi <amirthehossein@gmail.com>
2022-02-14 09:44:23 +0330

    defined models for courseapp

--- src/homework/courseapp/models.py
+++ src/homework/courseapp/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Course(models.Model):
+    name = models.CharField('course name', max_length=128)
+    teacher = models.ForeignKey('userapp.Teacher', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name} by {self.creator.name}'
+
+
+class PersentationStudentRel(models.Model):
+    student = models.ForeignKey('userapp.Student', on_delete=models.CASCADE)
+    # TODO: if the student object is deleted, will it cause the presentation
+    # object to delete?!
+    presentation = models.ForeignKey('courseapp.Presentation',
+                                     on_delete=models.CASCADE)
+    grade = models.IntegerField('course grade', blank=True)
+    passed = models.BooleanField(default=False)
+
+
+class Presentation(models.Model):
+    start_date = models.DateField('start date')
+    end_date = models.DateField('end date')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    students = models.ManyToManyField('userapp.Student',
+                                      through=PersentationStudentRel)
+
+    def __str__(self):
+        return f'{str(self.course)} from {self.start_date} to {self.end_date}'
