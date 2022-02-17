82ad2c40d640ad13060b89517a46ad7d868a7f39
amirhossein <amirthehossein@gmail.com>
2022-02-17 14:11:03 +0330

    cleaned up tests to test the written code, not the framework code

--- src/homework/courseapp/tests.py
+++ src/homework/courseapp/tests.py
@@ -1,32 +1,23 @@
-from datetime import timedelta, date
+from datetime import timedelta
 
 from django.test import TestCase
 from django.utils.timezone import now
 
-from .models import Course, Presentation, PersentationStudentRel
-from userapp.models import Teacher, Student
+from .models import Course, Presentation
+from userapp.models import Teacher
 # Create your tests here.
 
 
 class CourseModelTest(TestCase):
     def setUp(self):
         Teacher.objects.create(username='dummyteacher', name='Dummy Teacher')
-        teacher_obj = Teacher.objects.get(username='dummyteacher')
-        Course.objects.create(name='Dummy Course', teacher=teacher_obj)
-
-    def test_datatype_of_fields(self):
-        course_obj = Course.objects.get(name='Dummy Course')
-        self.assertIsInstance(course_obj.name, str)
-        self.assertIsInstance(course_obj.teacher, Teacher)
-
-    def test_teacher_relationship(self):
-        teacher_obj = Teacher.objects.get(username='dummyteacher')
-        course_obj = Course.objects.get(name='Dummy Course')
-        self.assertEqual(course_obj.teacher, teacher_obj)
+        Course.objects.create(name='Dummy Course',
+                              teacher=Teacher.objects.get(
+                                  username='dummyteacher'))
 
     def test_string_format(self):
-        course_obj = Course.objects.get(name='Dummy Course')
-        self.assertEqual(str(course_obj), 'Dummy Course by Dummy Teacher')
+        self.assertEqual(str(Course.objects.get(name='Dummy Course')),
+                         'Dummy Course by Dummy Teacher')
 
 
 class PresentationModelTest(TestCase):
@@ -39,23 +30,6 @@ class PresentationModelTest(TestCase):
                                     timedelta(days=3),
                                     end_date=now().date() + timedelta(days=3),
                                     course=course_obj)
-        presentation_obj = Presentation.objects.get(pk=1)
-        Student.objects.create(username='dummystudent', name='Dummy Student')
-        student_obj = Student.objects.get(username='dummystudent')
-        PersentationStudentRel.objects.create(student=student_obj,
-                                              presentation=presentation_obj)
-
-    def test_datatype_of_fields(self):
-        presentation_obj = Presentation.objects.get(pk=1)
-        self.assertIsInstance(presentation_obj.start_date, date)
-        self.assertIsInstance(presentation_obj.end_date, date)
-        self.assertIsInstance(presentation_obj.course, Course)
-        self.assertIsInstance(presentation_obj.students.get(pk=1), Student)
-
-    def test_students_relationship(self):
-        student_obj = Student.objects.get(username='dummystudent')
-        presentation_obj = Presentation.objects.get(pk=1)
-        self.assertEqual(presentation_obj.students.get(pk=1), student_obj)
 
     def test_string_format(self):
         presentation_obj = Presentation.objects.get(pk=1)
--- src/homework/userapp/tests.py
+++ src/homework/userapp/tests.py
@@ -1,7 +1,4 @@
-from datetime import date
-
 from django.test import TestCase
-from django.utils import timezone
 
 # Create your tests here.
 
@@ -12,16 +9,6 @@ class TeacherModelTest(TestCase):
     def setUp(self):
         Teacher.objects.create(username='dummyteacher', name='Dummy Teacher')
 
-    def test_datatypes_of_fields(self):
-        teacher_obj = Teacher.objects.get(username='dummyteacher')
-        self.assertIsInstance(teacher_obj.username, str)
-        self.assertIsInstance(teacher_obj.name, str)
-        self.assertIsInstance(teacher_obj.registration_date, date)
-
-    def test_value_of_registration_date_field(self):
-        teacher_obj = Teacher.objects.get(username='dummyteacher')
-        self.assertEqual(teacher_obj.registration_date, timezone.now().date())
-
     def test_string_format(self):
         self.assertEqual(str(Teacher.objects.get(username='dummyteacher')),
                          'Dummy Teacher')
@@ -30,16 +17,6 @@ class TeacherModelTest(TestCase):
 class StudentModelTest(TestCase):
     def setUp(self):
         Student.objects.create(username='dummystudent', name='Dummy Student')
-
-    def test_datatypes_of_fields(self):
-        student_obj = Student.objects.get(username='dummystudent')
-        self.assertIsInstance(student_obj.username, str)
-        self.assertIsInstance(student_obj.name, str)
-        self.assertIsInstance(student_obj.registration_date, date)
-
-    def test_value_of_registration_date_field(self):
-        student_obj = Student.objects.get(username='dummystudent')
-        self.assertEqual(student_obj.registration_date, timezone.now().date())
 
     def test_string_format(self):
         self.assertEqual(str(Student.objects.get(username='dummystudent')),
