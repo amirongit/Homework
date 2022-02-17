34d979f9acd24379abd8a2e3d11917ad082d332a
amirhossein <amirthehossein@gmail.com>
2022-02-17 11:49:14 +0330

    changed constraints for models of courseapp application

--- src/homework/courseapp/models.py
+++ src/homework/courseapp/models.py
@@ -5,7 +5,8 @@ from django.db import models
 
 class Course(models.Model):
     name = models.CharField('course name', max_length=128)
-    teacher = models.ForeignKey('userapp.Teacher', on_delete=models.CASCADE)
+    teacher = models.ForeignKey('userapp.Teacher', on_delete=models.CASCADE,
+                                null=False, blank=False)
 
     def __str__(self):
         return f'{self.name} by {self.teacher.name}'
@@ -17,13 +18,14 @@ class PersentationStudentRel(models.Model):
     # object to delete?!
     presentation = models.ForeignKey('courseapp.Presentation',
                                      on_delete=models.CASCADE)
-    grade = models.IntegerField('course grade', blank=True)
+    grade = models.IntegerField('course grade', blank=True, null=True)
 
 
 class Presentation(models.Model):
     start_date = models.DateField('start date')
     end_date = models.DateField('end date')
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, null=False,
+                               blank=False)
     students = models.ManyToManyField('userapp.Student',
                                       through=PersentationStudentRel)
 
