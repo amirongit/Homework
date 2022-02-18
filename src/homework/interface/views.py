00392a2dc48a82ba6bd1d05dd8e197538c4541b9
amirhossein <amirthehossein@gmail.com>
2022-02-18 16:12:56 +0330

    added index view to interface app

--- src/homework/interface/views.py
+++ src/homework/interface/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+import imp
+from django.views import generic
+from django.shortcuts import HttpResponse
 
 # Create your views here.
+
+
+class Index(generic.TemplateView):
+    template_name = 'interface/index.html'
