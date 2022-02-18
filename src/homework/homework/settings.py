e29f1b72439d0ddd9327fec56e414b233214551f
amirhossein <amirthehossein@gmail.com>
2022-02-18 16:14:11 +0330

    added interface app to installed apps

--- src/homework/homework/settings.py
+++ src/homework/homework/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'interface.apps.InterfaceConfig',
     'userapp.apps.UserappConfig',
     'courseapp.apps.CourseappConfig',
     'django.contrib.admin',
