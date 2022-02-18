b25a76bf9d06fcb3174e2484245c628987b4c308
amirhossein <amirthehossein@gmail.com>
2022-02-18 18:28:43 +0330

    added password and email fields to register form of interface app

--- src/homework/interface/forms.py
+++ src/homework/interface/forms.py
@@ -4,7 +4,7 @@ from django import forms
 class RegisterForm(forms.Form):
     username = forms.CharField(label='Username', max_length=32, required=True)
     name = forms.CharField(label='Name', max_length=128, required=True)
-
+    email = forms.EmailField(label='Email', required=True)
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
     user_type = forms.ChoiceField(label='Role', choices=[('t', 'Teacher'),
                                                          ('s', 'Student')])
-
