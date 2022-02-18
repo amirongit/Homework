f9d0bc55458f11c2ab1eddf40877ab94c19e469b
amirhossein <amirthehossein@gmail.com>
2022-02-18 18:18:20 +0330

    added register form to interface app

--- src/homework/interface/forms.py
+++ src/homework/interface/forms.py
@@ -5,4 +5,6 @@ class RegisterForm(forms.Form):
     username = forms.CharField(label='Username', max_length=32, required=True)
     name = forms.CharField(label='Name', max_length=128, required=True)
 
-    user_type = forms.ChoiceField(label='Role', choices=[('t', 'Teacher'), ('s', 'Student')])
+    user_type = forms.ChoiceField(label='Role', choices=[('t', 'Teacher'),
+                                                         ('s', 'Student')])
+
