b490bd86e76fe338d625607b2080aeb7f16925e5
amirhossein <amirthehossein@gmail.com>
2022-02-19 15:58:04 +0330

    added required constraint to password field of the register form

--- src/homework/userapp/forms.py
+++ src/homework/userapp/forms.py
@@ -3,8 +3,12 @@ from django import forms
 
 class RegisterForm(forms.Form):
     username = forms.CharField(label='Username', max_length=32, required=True)
-    name = forms.CharField(label='Name', max_length=128, required=True)
+    first_name = forms.CharField(label='First Name', max_length=64,
+                                 required=True)
+    last_name = forms.CharField(label='Last Name', max_length=64,
+                                required=True)
     email = forms.EmailField(label='Email', required=True)
-    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password = forms.CharField(label='Password', widget=forms.PasswordInput,
+                               required=True)
     user_type = forms.ChoiceField(label='Role', choices=[('t', 'Teacher'),
                                                          ('s', 'Student')])
