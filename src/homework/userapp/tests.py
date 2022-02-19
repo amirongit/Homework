79046306ff188a0b4fed548162fe2d66a6a077b1
amirhossein <amirthehossein@gmail.com>
2022-02-19 14:55:12 +0330

    removed tests from userapp since there is nothing to test in the present

--- src/homework/userapp/tests.py
+++ src/homework/userapp/tests.py
@@ -1,23 +1,3 @@
 from django.test import TestCase
 
 # Create your tests here.
-
-from .models import Teacher, Student
-
-
-class TeacherModelTest(TestCase):
-    def setUp(self):
-        Teacher.objects.create(username='dummyteacher', name='Dummy Teacher')
-
-    def test_string_format(self):
-        self.assertEqual(str(Teacher.objects.get(username='dummyteacher')),
-                         'Dummy Teacher')
-
-
-class StudentModelTest(TestCase):
-    def setUp(self):
-        Student.objects.create(username='dummystudent', name='Dummy Student')
-
-    def test_string_format(self):
-        self.assertEqual(str(Student.objects.get(username='dummystudent')),
-                         'Dummy Student')
