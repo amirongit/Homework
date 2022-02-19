11b99cedeb116e212cf136254eb7aae496871c35
amirhossein <amirthehossein@gmail.com>
2022-02-19 14:52:47 +0330

    removed the defined user model and the abstract user model provided by django was used instead

--- src/homework/userapp/models.py
+++ src/homework/userapp/models.py
@@ -1,24 +1,10 @@
-from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
 
-class User(models.Model):
-
-    class Meta:
-        abstract = True
-
-    username = models.CharField(max_length=32, unique=True, blank=False,
-                                null=False)
-    name = models.CharField(max_length=128, blank=False, null=False)
-    email = models.EmailField(unique=True, blank=False, null=False,
-                              editable=False)
-    password = models.CharField(max_length=64, blank=False, null=False)
-    registration_date = models.DateField('registration date', null=False,
-                                         auto_now_add=True)
-
-    def __str__(self):
-        return self.name
+class User(AbstractUser):
+    pass
 
 
 class Teacher(User):
