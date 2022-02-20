7bfb7f8b79cb76fd4f09c21dd84b575646e8f18e
amirhossein <amirthehossein@gmail.com>
2022-02-20 16:15:15 +0330

    removed teacher and student models, created teacher and student proxy models for the user model, added managers for student and teacher proxy models

--- src/homework/userapp/models.py
+++ src/homework/userapp/models.py
@@ -1,3 +1,4 @@
+from django.db import models
 from django.contrib.auth.models import AbstractUser, UserManager
 
 # Create your models here.
@@ -6,10 +7,45 @@ from django.contrib.auth.models import AbstractUser, UserManager
 class User(AbstractUser):
     objects = UserManager()
 
+    class Types(models.TextChoices):
+        TEACHER = ('TEACHER', 'Teacher')
+        STUDENT = ('STUDENT', 'Student')
+
+    user_type = models.CharField(max_length=8, choices=Types.choices,
+                                 null=False, blank=False)
+
+
+class TeacherManager(UserManager):
+    def get_queryset(self, *args, **kwargs):
+        return super().get_queryset(*args, **kwargs).filter(
+            user_type=User.Types.TEACHER)
+
 
 class Teacher(User):
-    pass
+    class Meta:
+        proxy = True
+
+    objects = TeacherManager()
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.user_type = User.Types.TEACHER
+        super().save(*args, **kwargs)
+
+
+class StudentManager(UserManager):
+    def get_queryset(self, *args, **kwargs):
+        return super().get_queryset(*args, **kwargs).filter(
+            user_type=User.Types.STUDENT)
 
 
 class Student(User):
-    pass
+    class Meta:
+        proxy = True
+
+    objects = StudentManager()
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.user_type = User.Types.STUDENT
+        super().save(*args, **kwargs)
