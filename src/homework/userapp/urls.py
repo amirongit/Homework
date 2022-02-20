0bb983864e933133855c7180ebbdd5e9a77198ac
amirhossein <amirthehossein@gmail.com>
2022-02-20 11:53:38 +0330

    added login view to userapp urlconf

--- src/homework/userapp/urls.py
+++ src/homework/userapp/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'userapp'
 
 urlpatterns = [
-    path('register/', views.register, name='register')
+    path('register/', views.register, name='register'),
+    path('login/', views.LoginView_.as_view(), name='login')
 ]
