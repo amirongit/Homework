9326a7128477f3b5abbad8e5bdbf91ae5a70acb2
amirhossein <amirthehossein@gmail.com>
2022-02-21 20:40:07 +0330

    added course create view to courseapp urlconf

--- src/homework/courseapp/urls.py
+++ src/homework/courseapp/urls.py
@@ -6,5 +6,6 @@ app_name = 'courseapp'
 
 urlpatterns = [
     path('teacher_courses/', views.TeacherCoursesView.as_view(),
-         name='teacher_courses')
+         name='teacher_courses'),
+    path('course_create/', views.course_create, name='course_create')
 ]
