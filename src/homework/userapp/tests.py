85180a82b184b12fb589add6bcb39fec4237d24d
amirhossein <amirthehossein@gmail.com>
2022-02-21 11:55:46 +0330

    added tests for userapp models

--- src/homework/userapp/tests.py
+++ src/homework/userapp/tests.py
@@ -1,3 +1,44 @@
 from django.test import TestCase
 
+from .models import User, Teacher, Student
+
 # Create your tests here.
+
+
+class UserModelTest(TestCase):
+    def setUp(self):
+        User.objects.create_user(username='dummyuser',
+                                 email='dummyuser@email.com',
+                                 password='dummypassword', first_name='Dummy',
+                                 last_name='User',
+                                 user_type=User.Types.TEACHER)
+
+    def test_string_format(self):
+        self.assertEqual(str(User.objects.get(username='dummyuser')),
+                         'Teacher: Dummy User')
+
+
+class TeacherModelTest(TestCase):
+    def setUp(self):
+        Teacher.objects.create_user(username='dummyuser',
+                                    email='dummyuser@email.com',
+                                    password='dummypassword',
+                                    first_name='Dummy',
+                                    last_name='User')
+
+    def test_teacher_manager_queryset(self):
+        self.assertEqual(Teacher.objects.all()[0].user_type,
+                         User.Types.TEACHER)
+
+
+class StudentModelTest(TestCase):
+    def setUp(self):
+        Student.objects.create_user(username='dummyuser',
+                                    email='dummyuser@email.com',
+                                    password='dummypassword',
+                                    first_name='Dummy',
+                                    last_name='User')
+
+    def test_student_manager_queryset(self):
+        self.assertEqual(Student.objects.all()[0].user_type,
+                         User.Types.STUDENT)
