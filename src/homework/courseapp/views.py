175a39eeb0fafab98766fb867b06a39ea86c4dcc
amirhossein <amirthehossein@gmail.com>
2022-02-21 19:11:11 +0330

    added teacher courses view to courseapp views

--- src/homework/courseapp/views.py
+++ src/homework/courseapp/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 
+from userapp.models import Teacher
 # Create your views here.
+
+
+class TeacherCoursesView(LoginRequiredMixin, generic.ListView):
+    template_name = 'courseapp/teacher_courses.html'
+    context_object_name = 'course_set'
+
+    def get_queryset(self):
+        return Teacher.objects.get(pk=self.request.user.id).course_set.all()
