f37a2aa6ec923a949e6eb5e9909a0450957fb061
amirhossein <amirthehossein@gmail.com>
2022-02-22 10:58:50 +0330

    added new course view defenition to courseapp

--- src/homework/courseapp/views.py
+++ src/homework/courseapp/views.py
@@ -1,7 +1,13 @@
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import user_passes_test
 
-from userapp.models import Teacher
+from userapp.models import Teacher, User
+
+from .forms import CourseCreationForm
+from .utils import generate_user_type_test
 # Create your views here.
 
 
@@ -11,3 +17,17 @@ class TeacherCoursesView(LoginRequiredMixin, generic.ListView):
 
     def get_queryset(self):
         return Teacher.objects.get(pk=self.request.user.id).course_set.all()
+
+    def get_context_data(self, *args, **kwargs):
+        cxt = super().get_context_data(*args, **kwargs)
+        cxt.update({'title': 'My courses'})
+        return cxt
+
+
+@user_passes_test(generate_user_type_test(User.Types.TEACHER))
+def new_course(request):
+    if request.method == 'POST':
+        pass
+    form = CourseCreationForm()
+    return render(request, 'courseapp/new_course.html',
+                  {'title': 'New course', 'form': form})
