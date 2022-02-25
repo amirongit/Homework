e05a5bba84be951d0a3494d94e1d7f4343ad9fb8
amirhossein <amirthehossein@gmail.com>
2022-02-25 11:19:01 +0330

    changed sign up view in the userapp urlconf

--- src/homework/userapp/urls.py
+++ src/homework/userapp/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'userapp'
 
 urlpatterns = [
-    path('sign_up/', views.sign_up, name='sign_up'),
+    path('sign_up/', views.SignUpView.as_view(), name='sign_up'),
     path('sign_in/', views.SingInView.as_view(), name='sign_in'),
     path('sign_out/', views.SignOutView.as_view(), name='sign_out')
 ]
