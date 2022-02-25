c85a7e3b483351a5a8af059c80e7cfe1e7a5d355
amirhossein <amirthehossein@gmail.com>
2022-02-25 11:19:32 +0330

    extended sign up form by generic views

--- src/homework/userapp/views.py
+++ src/homework/userapp/views.py
@@ -1,30 +1,27 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.mixins import (LoginRequiredMixin,
                                         UserPassesTestMixin, AccessMixin)
-from django.contrib.auth.decorators import user_passes_test
-from django.db import IntegrityError
-from django.shortcuts import redirect, render
-from django.urls import reverse
+from django.shortcuts import redirect
+from django.urls import reverse, reverse_lazy
+from django.views import generic
 
-from .forms import SignUpForm, SignInForm
+from .forms import UserInfoForm, SignInForm
 
 # Create your views here.
 
 
-@user_passes_test(lambda user: user.is_anonymous)
-def sign_up(request):
-    if request.method == 'POST':
-        form = SignUpForm(request.POST)
-        if not form.is_valid():
-            return redirect(reverse('userapp:sign_up'))
-        try:
-            form.save()
-            return redirect(reverse('userapp:sign_in'))
-        except (IntegrityError, ValueError):
-            return redirect(reverse('userapp:sign_up'))
-    form = SignUpForm()
-    return render(request, 'userapp/sign_up.html', {'title': 'Sign up',
-                                                    'form': form})
+class SignUpView(UserPassesTestMixin, generic.CreateView):
+    template_name = 'userapp/sign_up.html'
+    form_class = UserInfoForm
+    success_url = reverse_lazy('userapp:sign_in')
+
+    def test_func(self):
+        return self.request.user.is_anonymous
+
+    def get_context_data(self, *args, **kwargs):
+        cxt = super().get_context_data(*args, **kwargs)
+        cxt.update({'title': 'Sign up'})
+        return cxt
 
 
 class SingInView(UserPassesTestMixin, AccessMixin, LoginView):
