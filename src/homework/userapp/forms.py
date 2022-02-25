471c77e4a6f1ac27d845b522986304c792a32f46
amirhossein <amirthehossein@gmail.com>
2022-02-25 11:18:46 +0330

    renamed sign up form to user info form

--- src/homework/userapp/forms.py
+++ src/homework/userapp/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
-class SignUpForm(UserCreationForm):
+class UserInfoForm(UserCreationForm):
     class Meta:
         model = get_user_model()
         fields = ('username', 'password1', 'password2', 'first_name',
