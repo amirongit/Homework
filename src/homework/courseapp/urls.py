6646f2f58b27e9d33142cb2f226419612f9f30e6
amirhossein <amirthehossein@gmail.com>
2022-02-25 11:04:38 +0330

    changed the new course view in urlconf of the courseapp

--- src/homework/courseapp/urls.py
+++ src/homework/courseapp/urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
     path('teacher/', include([
         path('courses/', views.TeacherCoursesView.as_view(),
              name='teacher_courses'),
-        path('new_course/', views.new_course, name='teacher_new_course')
+        path('new_course/', views.NewCourseView.as_view(),
+             name='teacher_new_course')
     ])),
     path('student/', include([
     ]))
