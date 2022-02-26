6633c934eeead1432d278427f84bb97cfed5bfcd
amirhossein <amirthehossein@gmail.com>
2022-02-26 11:36:55 +0330

    added new presentation view to courseapp urlconf

--- src/homework/courseapp/urls.py
+++ src/homework/courseapp/urls.py
@@ -9,14 +9,19 @@ urlpatterns = [
         path('courses/', views.TeacherCoursesView.as_view(),
              name='teacher_courses'),
         path('new_course/', views.NewCourseView.as_view(),
-             name='teacher_new_course')
+             name='teacher_new_course'),
+        path('course/', include([
+          path('update/<int:pk>/', views.CourseUpdateView.as_view(),
+               name='course_update'),
+          path('new_presentation/<int:course_id>/',
+               views.NewPresentationView.as_view(),
+               name='course_new_presentation')
+        ]))
     ])),
     path('student/', include([
     ])),
     path('course/', include([
         path('details/<int:pk>/', views.CourseDetailView.as_view(),
              name='course_details'),
-        path('update/<int:pk>/', views.CourseUpdateView.as_view(),
-             name='course_update')
     ])),
 ]
