9ae73d19f5fb6f09b8abf7628197171c85b05bbb
amirhossein <amirthehossein@gmail.com>
2022-02-27 23:03:02 +0330

    added utils module to userapp to provide user specific type mixins for other apps

---
+++ src/homework/userapp/utils.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
+from .models import User
+
+
+class TeacherOnlyViewMixin(LoginRequiredMixin, UserPassesTestMixin):
+    def test_func(self):
+        return self.request.user.user_type == User.Types.TEACHER
+
+
+class StudnetOnlyViewMixin(LoginRequiredMixin, UserPassesTestMixin):
+    def test_func(self):
+        return self.request.user.user_type == User.Types.TEACHER
