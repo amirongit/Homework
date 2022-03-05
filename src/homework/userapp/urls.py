c0bfae70928ec29bb6be0143bbbcc8d9c7c5bde1
amirhossein <amirthehossein@gmail.com>
2022-03-05 11:21:01 +0330

    added teacher profile view to userapp urlconf

--- src/homework/userapp/urls.py
+++ src/homework/userapp/urls.py
@@ -7,5 +7,7 @@ app_name = 'userapp'
 urlpatterns = [
     path('sign_up/', views.SignUpView.as_view(), name='sign_up'),
     path('sign_in/', views.SingInView.as_view(), name='sign_in'),
-    path('sign_out/', views.SignOutView.as_view(), name='sign_out')
+    path('sign_out/', views.SignOutView.as_view(), name='sign_out'),
+    path('profile/<int:pk>/', views.TeacherProfileView.as_view(),
+         name='teacher_profile')
 ]
