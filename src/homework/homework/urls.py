608668f64e2c90e8ca1236bd510557e4d4d6fda2
amirhossein <amirthehossein@gmail.com>
2022-03-06 15:28:02 +0330

    added django admin urls to the root urlconf

--- src/homework/homework/urls.py
+++ src/homework/homework/urls.py
@@ -13,10 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('', include('interface.urls')),
     path('users/', include('userapp.urls')),
-    path('courses/', include('courseapp.urls'))
+    path('courses/', include('courseapp.urls')),
+    path('admin/', admin.site.urls)
 ]
