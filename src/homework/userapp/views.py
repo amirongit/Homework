e129d7cdc2e51302a6feb4d7676f0d959a32c398
amirhossein <amirthehossein@gmail.com>
2022-03-06 15:29:39 +0330

    improved the use of utility mixins in userapp views

--- src/homework/userapp/views.py
+++ src/homework/userapp/views.py
@@ -1,7 +1,6 @@
-from django.contrib.auth.mixins import AccessMixin, LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
-from django.shortcuts import redirect
-from django.urls import reverse, reverse_lazy
+from django.urls import reverse_lazy
 from django.views import generic
 
 from .forms import SignInForm, UserInfoForm
@@ -16,21 +15,15 @@ class SignUpView(AnonymousOnlyViewMixin, generic.CreateView):
     form_class = UserInfoForm
     success_url = reverse_lazy('userapp:sign_in')
 
-    def handle_no_permission(self):
-        return redirect(reverse('interface:index'))
-
     def get_context_data(self, *args, **kwargs):
         cxt = super().get_context_data(*args, **kwargs)
         cxt.update({'title': 'Sign up'})
         return cxt
 
 
-class SingInView(AnonymousOnlyViewMixin, AccessMixin, LoginView):
+class SingInView(AnonymousOnlyViewMixin, LoginView):
     template_name = 'userapp/sign_in.html'
     form_class = SignInForm
-
-    def handle_no_permission(self):
-        return redirect(reverse('interface:index'))
 
     def get_context_data(self, *args, **kwargs):
         cxt = super().get_context_data(*args, **kwargs)
