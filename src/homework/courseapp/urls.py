fcb25bb2bf0b83f6618617be293ea364ec265f21
amirhossein <amirthehossein@gmail.com>
2022-03-12 13:55:39 +0330

    added new lecture view to courseapp urlconf

--- src/homework/courseapp/urls.py
+++ src/homework/courseapp/urls.py
@@ -7,14 +7,11 @@ app_name = 'courseapp'
 urlpatterns = [
     path('teacher/', include(
           [
-           path('courses/',
-                views.TeacherCoursesView.as_view(),
+           path('courses/', views.TeacherCoursesView.as_view(),
                 name='teacher_courses'),
-           path('new_course/',
-                views.NewCourseView.as_view(),
+           path('new_course/', views.NewCourseView.as_view(),
                 name='new_course'),
-           path('update_course/<int:pk>/',
-                views.CourseUpdateView.as_view(),
+           path('update_course/<int:pk>/', views.CourseUpdateView.as_view(),
                 name='update_course'),
            path('new_presentation/<int:course_id>/',
                 views.NewPresentationView.as_view(),
@@ -31,12 +28,14 @@ urlpatterns = [
            path('homework_details/<int:pk>/',
                 views.HomeworkDetailsView.as_view(),
                 name='homework_details'),
-           path('update_grade/<int:pk>/',
-                views.UpdateGradeView.as_view(),
+           path('update_grade/<int:pk>/', views.UpdateGradeView.as_view(),
                 name='update_grade'),
            path('attendancy_details/<int:pk>/',
                 views.AttendancyDetailsView.as_view(),
-                name='attendancy_details')
+                name='attendancy_details'),
+           path('new_lecture/<int:presentation_id>/',
+                views.NewLectureView.as_view(),
+                name='new_lecture')
           ]
                )),
     path('student/', include(
@@ -47,8 +46,7 @@ urlpatterns = [
            path('submit_answer/<int:homework_id>/',
                 views.SubmitAnswerView.as_view(),
                 name='submit_answer'),
-           path('courses/',
-                views.StudentCoursesView.as_view(),
+           path('courses/', views.StudentCoursesView.as_view(),
                 name='student_courses'),
            path('course_dashboard/<int:pk>/',
                 views.PresentationDashboardView.as_view(),
